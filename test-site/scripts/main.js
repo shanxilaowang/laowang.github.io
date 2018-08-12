dc6d7dcc7af487831f0c164f0332a5feefcf10f2
laowong <wangjingsky@outlook.com>
2018-08-12 14:31:11 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,35 @@
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+// var myVariable;
+// myVariable = 'Bob';
+// //上面两行也可以写成 var myVariable = 'Bob';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/1.jpg') {
+      myImage.setAttribute('src', 'images/2.png');
+    } else {
+      myImage.setAttribute('src', 'images/1.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
